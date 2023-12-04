063ff060b3865a9ab89b97d247abb2898c360b35
Yaathavan <yaathavan.kumarasamy@etu.utc.fr>
2023-12-04 16:48:58 +0100

    commit test

--- src/Routes.js
+++ src/Routes.js
@@ -4,12 +4,14 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import InternHomePage from "./pages/InternHomePage/InternHomePage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
 
 function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
             <Route path="/intern-home" element={<InternHomePage />} />
         </Routes>
     );
