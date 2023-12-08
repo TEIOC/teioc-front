79a49597ed6ecfcae3b26e2b698b3d5fd44c7efb
abdel <dorra.abdelkefi@etu.utc.fr>
2023-12-08 11:56:03 +0100

    logout to login page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/navbar.css';
 
-function NavBar({ isLoggedIn }) {
+function NavBar({ isLoggedIn, onLogout }) {
     return (
         <div className="header-banner">
             <h1>TEIOC - Assessment of Interns Platform</h1>
             {isLoggedIn
-                ? <Link to="/logout" className="navbar-button">Logout</Link>
+                ? <button onClick={onLogout} className="navbar-button">Logout</button>
                 : <Link to="/login" className="navbar-button">Login</Link>
             }
         </div>
@@ -15,5 +15,6 @@ function NavBar({ isLoggedIn }) {
 }
 
 export default NavBar;
+
 
 
--- src/pages/InternHomePage/InternHomePage.js
+++ src/pages/InternHomePage/InternHomePage.js
@@ -1,13 +1,22 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import NavBar from "../../components/NavBar";
 import Separator from "../../components/Separator";
 import SurveyWithTopicList from "../../components/SurveyWithTopicList";
 
 function InternHomePage({ internName }) {
-    const [isLoggedIn] = useState(true);
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        setIsLoggedIn(false);
+        localStorage.removeItem("userToken");
+        navigate('/login');
+    };
+
     return (
         <div className="home-page">
-            <NavBar isLoggedIn={isLoggedIn} />
+            <NavBar isLoggedIn={isLoggedIn} onLogout={handleLogout} />
             <Separator />
             <SurveyWithTopicList />
         </div>
@@ -15,3 +24,4 @@ function InternHomePage({ internName }) {
 }
 
 export default InternHomePage;
+
