9de670e7a965b3c1bdeb09fe69b1ccd5239008e3
abdel <dorra.abdelkefi@etu.utc.fr>
2023-12-10 23:46:08 +0100

    cleaner structure

--- src/components/Separator.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import '../styles/separator.css';
-function Separator() {
-    return (
-        <div className="list-separator"></div>
-    );
-}
-
-export default Separator;
