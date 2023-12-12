db592e09cf861477c6a2ba139b65b15ea1e1e9e8
abdel <dorra.abdelkefi@etu.utc.fr>
2023-12-12 19:02:45 +0100

    sidebar

--- src/components/navigation/InternHomeSidebar.js
+++ src/components/navigation/InternHomeSidebar.js
@@ -20,9 +20,10 @@ const InternHomeSidebar = () => {
         <div className="sidebar">
             <ul>
                 <li className="title">{name}</li>
-                <li><Link to="/intern-home">Home</Link></li>
-                <li><Link to="/account">My Account</Link></li>
-                <li><Link to="/results">My Results</Link></li>
+                <li><Link to="/intern-home">Surveys</Link></li>
+                <li><Link to="/account">Account settings</Link></li>
+                <li><Link to="/assessments">Completed Assessments</Link></li>
+                <li><Link to="/results">Results</Link></li>
             </ul>
         </div>
     );
