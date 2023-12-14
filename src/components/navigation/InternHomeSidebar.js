8260d79406f9dc908aeac9bc6f12d75ce447007a
abdel <dorra.abdelkefi@etu.utc.fr>
2023-12-14 09:48:20 +0100

    default profile pic

--- src/components/navigation/InternHomeSidebar.js
+++ src/components/navigation/InternHomeSidebar.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../styles/sidebar.css';
 import GetLoggedinIntern from '../../hooks/GetLoggedinIntern';
+import DefaultProfilePic from '../../styles/DefaultProfilePic.png';
 
 const InternHomeSidebar = () => {
     const intern = GetLoggedinIntern();
 
     return (
         <div className="sidebar">
+            <img src={DefaultProfilePic} alt="Profile" className="profile-pic"/>
             <ul>
                 <li className="title">{intern ? intern.name : ''}</li>
                 <li><Link to="/account-settings">Account Settings</Link></li>
