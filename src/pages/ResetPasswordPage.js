2052a6d15f70258976d6cca6a59ba72c3b83e189
Yaathavan Kumarasamy <yaathavan.kumarasamy@etu.utc.fr>
2023-12-15 13:32:52 +0100

    clean code

--- src/pages/ResetPasswordPage.js
+++ src/pages/ResetPasswordPage.js
@@ -1,19 +1,8 @@
-import { React, useState } from 'react';
-import NavBar from "../components/navigation/NavBar";
+import { React } from 'react';
 import Separator from "../components/navigation/Separator";
 import ResetPasswordForm from "../components/forms/ResetPasswordForm";
-import { useNavigate } from 'react-router-dom';
-import { logout } from '../services/AuthService';
 
 function ResetPasswordPage() {
-    const [isLoggedIn, setIsLoggedIn] = useState(true);
-    const navigate = useNavigate();
-    const handleLogout = () => {
-        setIsLoggedIn(false);
-        logout();
-        navigate("/login");
-    };
-
     return (
         <div>
             <Separator />
