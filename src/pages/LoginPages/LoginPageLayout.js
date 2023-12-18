b6d621ab71b02ce7f6b13c0362d2a8f6159a729d
Mostafa <53868779+MostafaGhalia@users.noreply.github.com>
2023-12-18 21:43:24 +0100

    Added Guest Side Bar on LoginPageLayout

--- src/pages/LoginPages/LoginPageLayout.js
+++ src/pages/LoginPages/LoginPageLayout.js
@@ -1,6 +1,7 @@
 import NavBar from "../../components/navigation/NavBar";
 import Separator from "../../components/navigation/Separator";
 import React from "react";
+import GuestHomeSidebar from "../../components/navigation/GuestHomeSidebar";
 
 export const LoginPageLayout = ({ children }) => {
 
@@ -8,6 +9,7 @@ export const LoginPageLayout = ({ children }) => {
         <div>
             <NavBar />
             <Separator />
+            <GuestHomeSidebar />
             <div>
                 {children}
             </div>
