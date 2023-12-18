4ed6803eeceae5f361938b3e12226512f4479349
abdel <dorra.abdelkefi@etu.utc.fr>
2023-12-18 18:01:10 +0100

    stats

--- src/components/charts/InternStatisticsChart.js
+++ src/components/charts/InternStatisticsChart.js
@@ -6,7 +6,9 @@ import {
     fetchIndividualPerformance,
     fetchTopicWisePerformance,
     fetchTopicWisePerformanceForIntern,
-    fetchSurveyWisePerformance
+    fetchSurveyWisePerformance,
+    fetchSurveyPerformanceForIntern,
+    fetchTopicPerformanceForIntern
 } from '../../services/Api';
 import GetLoggedinIntern from '../../hooks/GetLoggedinIntern';
 import '../../styles/chart.css';
@@ -14,14 +16,19 @@ import '../../styles/chart.css';
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 const InternStatisticsChart = () => {
+
     const intern = GetLoggedinIntern();
 
     const [overallPerformance, setOverallPerformance] = useState(null);
     const [individualPerformance, setIndividualPerformance] = useState(null);
     const [topicWisePerformanceData, setTopicWisePerformanceData] = useState({});
     const [topicWisePerformanceForInternData, setTopicWisePerformanceForInternData] = useState({});
+    const [topicWisePerformanceForIntern, setTopicWisePerformanceForIntern] = useState({});
     const [surveyWisePerformanceData, setSurveyWisePerformanceData] = useState({});
+    const [surveyPerformanceForIntern, setSurveyPerformanceForIntern] = useState({});
 
+
+// À l'intérieur de la fonction useEffect
     useEffect(() => {
         const loadStatistics = async () => {
             const overallPerf = await fetchOverallPerformance();
@@ -29,12 +36,24 @@ const InternStatisticsChart = () => {
             const topicPerf = await fetchTopicWisePerformance();
             const topicPerfForIntern = await fetchTopicWisePerformanceForIntern(intern.id);
             const surveyPerf = await fetchSurveyWisePerformance();
+            const surveyPerfForIntern = await fetchSurveyPerformanceForIntern(intern.id);
+            const topicWisePerformanceForIntern = await fetchTopicPerformanceForIntern(intern.id);
+
+            console.log("Overall Performance:", overallPerf);
+            console.log("Individual Performance:", individualPerf);
+            console.log("Topic Performance:", topicPerf);
+            console.log("Topic Performance for Intern:", topicPerfForIntern);
+            console.log("Survey Performance:", surveyPerf);
+            console.log("Survey Performance for Intern:", surveyPerfForIntern);
+            console.log("Topic Performance for Intern:", topicWisePerformanceForIntern);
 
             setOverallPerformance(overallPerf);
             setIndividualPerformance(individualPerf);
             setTopicWisePerformanceData(topicPerf);
             setTopicWisePerformanceForInternData(topicPerfForIntern);
             setSurveyWisePerformanceData(surveyPerf);
+            setSurveyPerformanceForIntern(surveyPerfForIntern);
+            setTopicWisePerformanceForIntern(topicWisePerformanceForIntern);
         };
 
         if (intern && intern.id) {
@@ -42,7 +61,9 @@ const InternStatisticsChart = () => {
         }
     }, [intern]);
 
-    const formatChartData = (data) => {
+// Dans la fonction formatScoreChartData
+    const formatScoreChartData = (data) => {
+        console.log("Data for formatScoreChartData:", data);
         return {
             labels: Object.keys(data),
             datasets: [{
@@ -52,6 +73,32 @@ const InternStatisticsChart = () => {
             }],
         };
     };
+
+// Dans la fonction formatScoreDurationChartData
+    const formatScoreDurationChartData = (data) => {
+        console.log("Data for formatScoreDurationChartData:", data);
+        const labels = Object.keys(data);
+        const scores = labels.map(label => data[label]["Average Score"]);
+        const durations = labels.map(label => data[label]["Average Duration"]);
+
+        return {
+            labels,
+            datasets: [
+                {
+                    label: 'Average Score',
+                    data: scores,
+                    backgroundColor: 'rgba(53, 162, 235, 0.5)',
+                },
+                {
+                    label: 'Average Duration',
+                    data: durations,
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                }
+            ],
+        };
+    };
+
+
 
     return (
         <div className="statistics-container">
@@ -67,16 +114,24 @@ const InternStatisticsChart = () => {
                     <p>{individualPerformance}</p>
                 </div>
                 <div className="chart-container">
+                    <h3>Survey Performance for Intern</h3>
+                    <Bar data={formatScoreDurationChartData(surveyPerformanceForIntern)} />
+                </div>
+                <div className="chart-container">
+                    <h3>Topic Performance for Intern</h3>
+                    <Bar data={formatScoreDurationChartData(topicWisePerformanceForIntern)} />
+                </div>
+                <div className="chart-container">
                     <h3>Individual Topic-wise Performance</h3>
-                    <Bar data={formatChartData(topicWisePerformanceForInternData)} />
+                    <Bar data={formatScoreChartData(topicWisePerformanceForInternData)} />
                 </div>
                 <div className="chart-container">
                     <h3>Overall Topic-wise Performance</h3>
-                    <Bar data={formatChartData(topicWisePerformanceData)} />
+                    <Bar data={formatScoreChartData(topicWisePerformanceData)} />
                 </div>
                 <div className="chart-container">
                     <h3>Overall Survey-wise Performance</h3>
-                    <Bar data={formatChartData(surveyWisePerformanceData)} />
+                    <Bar data={formatScoreChartData(surveyWisePerformanceData)} />
                 </div>
             </div>
         </div>
