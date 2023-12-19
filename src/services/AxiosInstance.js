21f60f47f3bf06f0649d52f94b2a8e3570d716f9
Yaathavan Kumarasamy <yaathavan.kumarasamy@etu.utc.fr>
2023-12-19 22:30:11 +0100

    clean code

--- src/services/AxiosInstance.js
+++ src/services/AxiosInstance.js
@@ -1,6 +1,4 @@
 import axios from 'axios';
-import redirectToLogin from './RedirectToLogin.js'; // Assurez-vous que le chemin est correct
-import { jwtDecode } from "jwt-decode";
 
 const axiosInstance = axios.create({
     baseURL: 'http://localhost:8080',
@@ -23,47 +21,5 @@ axiosInstance.interceptors.request.use((config) => {
 }, (error) => {
     return Promise.reject(error);
 });
-
-// axiosInstance.interceptors.response.use(
-//     (response) => response,
-//     async (error) => {
-//         const originalRequest = error.config;
-//         if (error.response.status === 401 && !originalRequest._retry) {
-//             originalRequest._retry = true;
-//             try {
-//                 const refreshToken = localStorage.getItem('refreshToken');
-//                 if (!refreshToken) {
-//                     redirectToLogin();
-//                     return Promise.reject(error);
-//                 }
-
-//                 // Vérifiez si le refreshToken est expiré
-//                 const decoded = jwtDecode(refreshToken);
-//                 if (decoded.exp * 1000 < Date.now()) {
-//                     localStorage.removeItem('jwt');
-//                     localStorage.removeItem('refreshToken');
-//                     redirectToLogin();
-//                     return Promise.reject(error); // Le refreshToken est expiré
-//                 }
-
-//                 const response = await axiosInstance.post('/auth/refresh-token', { refreshToken });
-//                 const { token, refreshToken: newRefreshToken } = response.data;
-
-//                 localStorage.setItem('jwt', token);
-//                 localStorage.setItem('refreshToken', newRefreshToken);
-
-//                 originalRequest.headers.Authorization = `Bearer ${token}`;
-//                 return axiosInstance(originalRequest);
-//             } catch (refreshError) {
-//                 console.error(refreshError);
-//                 localStorage.removeItem('jwt');
-//                 localStorage.removeItem('refreshToken');
-//                 redirectToLogin();
-//                 return Promise.reject(refreshError); // Modifié pour renvoyer l'erreur
-//             }
-//         }
-//         return Promise.reject(error);
-//     }
-// );
 
 export default axiosInstance;
